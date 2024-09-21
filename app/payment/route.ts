91af3a409687fec9b12c6a6488f9d87d30a6f70c
Bruno Correa <brunosantos6ft@gmail.com>
2024-09-20 21:05:41 -0300

    fix: Use correct types for next request and response

--- app/payment/route.ts
+++ app/payment/route.ts
@@ -1,11 +1,12 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
 import { PAYMENT_STATUSES } from "../utils/payment-statuses";
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   const { searchParams } = new URL(req.url as string);
   const type = searchParams.get("type") || null;
   const amount = parseFloat(searchParams.get("amount") || "0");
